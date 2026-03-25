1a1dba84a04ab8208d138875c3ffe58128b0be46
SergioRodriguezEnt <SergioRodriguezEnt@users.noreply.github.com>
2026-03-25 13:42:46 +0000

    Prettified Code!

--- src/templates/js/register-form.js
+++ src/templates/js/register-form.js
@@ -1,9 +1,14 @@
 async function loadRegisterForm(data) {
   Promise.all([
-      loadContentForField("username-field", data),
-      loadContentForField("password-field", data),
-      loadContentForField("password-2-field", data),
-      loadContentFor(".register-form-btn", "textContent", data, "register-form-btn"),
+    loadContentForField("username-field", data),
+    loadContentForField("password-field", data),
+    loadContentForField("password-2-field", data),
+    loadContentFor(
+      ".register-form-btn",
+      "textContent",
+      data,
+      "register-form-btn",
+    ),
   ]).catch(console.error);
 }
 
