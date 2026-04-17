670d3a860a44632e78396d6cfc0526bd8fb6b485
Sergio Rodríguez <sergiorodriguezent@gmail.com>
2026-04-17 12:50:19 -0400

    a

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,9 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
   ]
 };
